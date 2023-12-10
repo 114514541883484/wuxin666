9de38d2a284164021896e941f0de510eaf3de8d6
114514541883484 <151984905+114514541883484@users.noreply.github.com>
2023-12-10 09:17:00 +0800

    Add files via upload

---
+++ wuxinnb.lua
@@ -0,0 +1,180 @@
+local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/jrdKakD6"))()
+
+
+local Window = OrionLib:MakeWindow({Name = "雾新脚本", HidePremium = false, SaveConfig = false, IntroText = "注入器："..identifyexecutor(), ConfigFolder = "雾新脚本"})
+
+OrionLib:MakeNotification({
+    Name = "雾新",
+    Content = "作者:静新/狗蛋",
+    Image = "rbxassetid://630235992",
+    Time = 6
+})
+
+local about = Window:MakeTab({
+    Name = "静新/狗蛋制作",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+about:AddParagraph("脚本为免费脚本 请勿去圈钱")
+about:AddParagraph("禁止倒卖")
+about:AddParagraph("狗蛋跟我一起合作")
+
+local Tab = Window:MakeTab({
+	Name = "doors脚本",
+	Icon = "rbxassetid://14503609101",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "doors",
+	Callback = function()
+      	loadstring(game:HttpGet("https://github.com/114514541883484/wuxin666/blob/main/doors%E8%84%9A%E6%9C%AC.txt",true))()
+  	end    
+})
+
+local Tab = Window:MakeTab({
+	Name = "一路向西脚本",
+	Icon = "rbxassetid://14503609101",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "一路向西",
+	Callback = function()
+      	loadstring(game:HttpGet("https://github.com/114514541883484/wuxin666/blob/main/%E4%B8%80%E8%B7%AF%E5%90%91%E8%A5%BF.lua",true))()
+  	end    
+})
+
+local Tab = Window:MakeTab({
+	Name = "极速传奇脚本",
+	Icon = "rbxassetid://14503609101",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "极速传奇",
+	Callback = function()
+      	loadstring(game:HttpGet("https://github.com/114514541883484/wuxin666/blob/main/%E9%80%9F%E5%BA%A6%E4%BC%A0%E5%A5%87%E8%84%9A%E6%9C%AC.lua",true))()
+  	end    
+})
+
+
+Tab:AddButton({
+	Name = "极速传奇2",
+	Callback = function()
+      	loadstring(game:HttpGet("https://github.com/114514541883484/wuxin66/blob/main/%E6%9E%81%E9%80%9F%E4%BC%A0%E5%A5%87.txt",true))()
+  	end    
+})
+
+
+local Tab = Window:MakeTab({
+	Name = "伐木脚本",
+	Icon = "rbxassetid://14503609101",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "伐木",
+	Callback = function()
+      	loadstring(game:HttpGet("https://github.com/114514541883484/wuxin666/blob/main/%E4%BC%90%E6%9C%A8.lua",true))()
+  	end    
+})
+
+local Tab = Window:MakeTab({
+	Name = "忍者脚本",
+	Icon = "rbxassetid://14503609101",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "忍者",
+	Callback = function()
+      	loadstring(game:HttpGet("https://github.com/114514541883484/wuxin666/blob/main/%E5%BF%8D%E8%80%85.lua",true))()
+  	end    
+})
+
+Tab:AddButton({
+	Name = "忍者2",
+	Callback = function()
+      	loadstring(game:HttpGet("https://github.com/114514541883484/wuxin66/blob/main/%E5%BF%8D%E8%80%85%E4%BC%A0%E5%A5%87.txt",true))()
+  	end    
+})
+
+local Tab = Window:MakeTab({
+	Name = "幸运方块脚本",
+	Icon = "rbxassetid://14503609101",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "幸运方块",
+	Callback = function()
+      	loadstring(game:HttpGet("https://github.com/114514541883484/wuxin666/blob/main/%E5%B9%B8%E8%BF%90%E6%96%B9%E5%9D%97.lua",true))()
+  	end    
+})
+
+local Tab = Window:MakeTab({
+	Name = "造船寻宝脚本",
+	Icon = "rbxassetid://14503609101",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "造船寻宝",
+	Callback = function()
+      	loadstring(game:HttpGet("https://github.com/114514541883484/wuxin666/blob/main/%E9%80%A0%E8%88%B9%E5%AF%BB%E5%AE%9D.lua",true))()
+  	end    
+})
+
+local Tab = Window:MakeTab({
+	Name = "蜂群脚本",
+	Icon = "rbxassetid://14503609101",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "蜂群",
+	Callback = function()
+      	loadstring(game:HttpGet("https://github.com/114514541883484/wuxin666/blob/main/%E8%9C%82%E7%BE%A4.lua",true))()
+  	end    
+})
+
+local Tab = Window:MakeTab({
+	Name = "力量脚本",
+	Icon = "rbxassetid://14503609101",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "力量",
+	Callback = function()
+      	loadstring(game:HttpGet("https://github.com/114514541883484/wuxin66/blob/main/%E5%8A%9B%E9%87%8F%E4%BC%A0%E5%A5%87.txt",true))()
+  	end    
+})
+
+local Tab = Window:MakeTab({
+	Name = "寻宝模拟器脚本",
+	Icon = "rbxassetid://14503609101",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "寻宝模拟器",
+	Callback = function()
+      	loadstring(game:HttpGet("https://github.com/114514541883484/wuxin66/blob/main/%E5%AF%BB%E5%AE%9D%E6%A8%A1%E6%8B%9F%E5%99%A8.txt",true))()
+  	end    
+})
+
+local Tab = Window:MakeTab({
+	Name = "洗衣模拟器脚本",
+	Icon = "rbxassetid://14503609101",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "洗衣",
+	Callback = function()
+      	loadstring(game:HttpGet("https://github.com/114514541883484/wuxin66/blob/main/%E6%B4%97%E8%A1%A3.txt",true))()
+  	end    
+})
